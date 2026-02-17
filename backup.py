fb0a827dbc758590b30c4ffeb82b0c88db213116
cq1026 <296056703@qq.com>
2026-02-17 09:45:25 +0800

    修复缺失的 os 模块导入

--- backup.py
+++ backup.py
@@ -6,6 +6,7 @@
 """
 
 import logging
+import os
 import shutil
 import subprocess
 from datetime import datetime
--- restore.py
+++ restore.py
@@ -6,6 +6,7 @@
 """
 
 import logging
+import os
 import shutil
 import sys
 from datetime import datetime
